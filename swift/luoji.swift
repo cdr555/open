6cff6f3eda33631950df3a5d659174d6b99cbd32
caidingrun <caidingrun@makeblock.com>
2025-05-09 15:56:56 +0800

    feat: 增加swift文件

---
+++ swift/luoji.swift
@@ -0,0 +1,27 @@
+let enteredDoorCode = true
+let passedRetinaScan = false
+if enteredDoorCode && passedRetinaScan {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
+
+let hasDoorKey = false
+let knowsOverridePassword = true
+if hasDoorKey || knowsOverridePassword {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
+
+// 复合条件 显示括号(enteredDoorCode && passedRetinaScan) || hasDoorKey || knowsOverridePassword
+if (enteredDoorCode && passedRetinaScan) || hasDoorKey || knowsOverridePassword {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
+
+let allowedEntry = false
+if !allowedEntry {
+    print("ACCESS DENIED")
+}
